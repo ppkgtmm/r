6045f9ef8f2ab0a831474c377acb7429586785dc
ppkgtmm <pkgtm1998@gmail.com>
2021-03-25 17:57:26 +0700

    edit import

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import BackgroundCard from "./background";
+import BackgroundCard from "./background/index.js";
 import Skills from "./skills";
 import Contact from "./Contact";
 import MainCard from "./MainCard";
