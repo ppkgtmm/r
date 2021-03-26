84c2af7561055c9b825e9deeb7d2cc8f458bd573
ppkgtmm <pkgtm1998@gmail.com>
2021-03-26 22:29:59 +0700

    edit tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,17 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      blue: "#E0F6F4",
-      orange: "#FBE4D3",
-      ygreen: "#EAF8DF", // yellow green
-      mgreen: "#E6F5EF", // mint green
-      yellow: "#FFF5D2",
-      bgrey: "#E3E7F3", // blue grey
-      grey: "#E5E5E5",
-      green: "#BFE0A5",
+    extend: {
+      colors: {
+        blue: "#E0F6F4",
+        orange: "#FBE4D3",
+        ygreen: "#EAF8DF", // yellow green
+        mgreen: "#E6F5EF", // mint green
+        yellow: "#FFF5D2",
+        bgrey: "#E3E7F3", // blue grey
+        grey: "#E5E5E5",
+        green: "#BFE0A5",
+      },
     },
   },
   variants: {
